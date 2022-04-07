7460ee3b137653a05d5ec8ed2c967f51ebe5c35c
Wang jing PhD DMAL <kningtg@scsegc.cm.cluster>
2022-04-07 21:53:01 +0800

    tmp commit Thu Apr  7 21:51:25 SGT 2022

--- .bashrc
+++ .bashrc
@@ -120,7 +120,7 @@ module load slurm
 alias sv="~/server_utils/server.sh"
 alias lsq="python ~/server_utils/list_task.py"
 alias fg="conda activate torch && python ~/server_utils/find_available_gpu.py"
-alias sg="conda activate torch && cd ~/server_utils && python ./scrumble_gpu.py"
+alias sg="conda activate torch && cd ~/server_utils && python ./train_model.py"
 alias nv='nvidia-smi --query-gpu=gpu_name,memory.total,memory.free --format=csv'
 timestamp=$(date)
 alias upl='git add . && git commit -m "tmp commit $timestamp" && git push'
@@ -151,3 +151,52 @@ export TMUX_TMPDIR="/export/home/kningtg/.tmp"
 gpu() {
   export CUDA_VISIBLE_DEVICES="$1"
 }
+ca() {
+    conda activate "$1"
+}
+
+export _HYDRA_OLD_COMP=$(complete -p python 2> /dev/null)
+hydra_bash_completion()
+{
+    words=($COMP_LINE)
+    if [ "${words[0]}" == "python" ]; then
+        if (( ${#words[@]} < 2 )); then
+            return
+        fi
+        file_path=$(pwd)/${words[1]}
+        if [ ! -f "$file_path" ]; then
+            return
+        fi
+        grep "@hydra.main" $file_path -q
+        helper="${words[0]} ${words[1]}"
+    else
+        helper="${words[0]}"
+        true
+    fi
+
+    EXECUTABLE=($(command -v $helper))
+    if [ "$HYDRA_COMP_DEBUG" == "1" ]; then
+        printf "EXECUTABLE_FIRST='${EXECUTABLE[0]}'\n"
+    fi
+    if ! [ -x "${EXECUTABLE[0]}" ]; then
+        false
+    fi
+
+    if [ $? == 0 ]; then
+        choices=$( COMP_POINT=$COMP_POINT COMP_LINE=$COMP_LINE $helper -sc query=bash)
+        word=${words[$COMP_CWORD]}
+
+        if [ "$HYDRA_COMP_DEBUG" == "1" ]; then
+            printf "\n"
+            printf "COMP_LINE='$COMP_LINE'\n"
+            printf "COMP_POINT='$COMP_POINT'\n"
+            printf "Word='$word'\n"
+            printf "Output suggestions:\n"
+            printf "\t%s\n" ${choices[@]}
+        fi
+        COMPREPLY=($( compgen -o nospace -o default -W "$choices" -- "$word" ));
+    fi
+}
+
+COMP_WORDBREAKS=${COMP_WORDBREAKS//=}
+COMP_WORDBREAKS=$COMP_WORDBREAKS complete -o nospace -o default -F hydra_bash_completion python
--- server_utils/train_model.py
+++ server_utils/train_model.py
@@ -24,8 +24,7 @@ def read_args():
     args = argparse.ArgumentParser()
     args.add_argument("--interval", type=float, default=0.1)
     # args.add_argument("--tensor_size", type=int, default=1, help="x 1000M")
-    args.add_argument("-m", "--mem_lim", type=float,
-                      default=np.inf, help="(G)")
+    args.add_argument("-m", "--mem_lim", type=float, default=np.inf, help="(G)")
     args.add_argument("-g", "--gpu_id", type=int, default=0)
     args.add_argument("-s", "--send_email", action="store_true")
     return args.parse_args()
@@ -37,9 +36,12 @@ def get_gpu_info(gpu_id):
     --format=csv,noheader,nounits
     """
 
-    ret_arr = subprocess.run(cmd, shell=True, capture_output=True,
-                             text=True).stdout.split("\n")[gpu_id].split(",")
-    if ret_arr[0] == '':
+    ret_arr = (
+        subprocess.run(cmd, shell=True, capture_output=True, text=True)
+        .stdout.split("\n")[gpu_id]
+        .split(",")
+    )
+    if ret_arr[0] == "":
         return None
     parsed_ret_arr = []
     for x in ret_arr:
@@ -48,8 +50,7 @@ def get_gpu_info(gpu_id):
         except:
             parsed_ret_arr.append(x)
 
-    columns = ["name", "utils", "util_mem",
-               "mem_total", "mem_free", "mem_used"]
+    columns = ["name", "utils", "util_mem", "mem_total", "mem_free", "mem_used"]
     info_dict = dict(zip(columns, parsed_ret_arr))
 
     return info_dict
@@ -59,8 +60,7 @@ def get_pid_info():
     global device
     pid = os.getpid()
     cmd = f"nvidia-smi | grep '{pid}      C'"
-    ret = subprocess.run(
-        cmd, shell=True, capture_output=True, text=True).stdout
+    ret = subprocess.run(cmd, shell=True, capture_output=True, text=True).stdout
     captured_mem = int(re.search("([0-9]*)MiB", ret).group(1))
 
     return captured_mem
@@ -69,26 +69,28 @@ def get_pid_info():
 def send_email(args):
     username = "scsegpu@163.com"
     password = "XTTCUQCCXXEYIVBG"
-    from_addr = "kningtg@163.com"
+    from_addr = "scsegpu@163.com"
     to_addr = "knjingwang@gmail.com"
-    hostname = subprocess.run("hostname", shell=True,
-                              capture_output=True, text=True).stdout
+    hostname = subprocess.run(
+        "hostname", shell=True, capture_output=True, text=True
+    ).stdout
     gpu_id = args.gpu_id
     gpu_info = get_gpu_info(gpu_id)
 
     subject = f"[SG] {hostname} GPU#{gpu_id}:{gpu_info['name']}"
-    text = pd.DataFrame.from_dict(gpu_info, orient="index").to_string(
-        header=False) + f"\nCaptured Memory: {get_pid_info()} Mb"
+    text = (
+        pd.DataFrame.from_dict(gpu_info, orient="index").to_string(header=False)
+        + f"\nCaptured Memory: {get_pid_info()} Mb"
+    )
 
     message = MIMEText(text, "plain", "utf-8")
-    message["From"] = formataddr(
-        (str(Header("GPU Cluster", "utf-8")), username))
+    message["From"] = formataddr((str(Header("GPU Cluster", "utf-8")), username))
     message["To"] = to_addr
     message["Subject"] = Header(subject, "utf-8")
 
     # print(message.as_string())
 
-    smtp_server = smtplib.SMTP_SSL('smtp.163.com', 465)
+    smtp_server = smtplib.SMTP_SSL("smtp.163.com", 465)
     smtp_server.login(username, password)
     smtp_server.sendmail(from_addr, to_addr, message.as_string())
     smtp_server.close()
@@ -105,8 +107,9 @@ def scrumble_gpu():
     tensor_list = []
 
     gpu_info = get_gpu_info(args.gpu_id)
-    hostname = subprocess.run("hostname", shell=True,
-                              capture_output=True, text=True).stdout
+    hostname = subprocess.run(
+        "hostname", shell=True, capture_output=True, text=True
+    ).stdout
     print(pd.DataFrame.from_dict(gpu_info, orient="index").to_string(header=False))
 
     mem_total = gpu_info["mem_total"]
@@ -116,8 +119,7 @@ def scrumble_gpu():
 
     # print(torch.cuda.)
     # import ipdb; ipdb.set_trace()
-    left_mem = 256
-    mem_lim = min(mem_total - left_mem, mem_lim)
+    mem_lim = min(mem_total * 0.96, mem_lim)
     disp_interval = 1000
     while True:
         try:
@@ -137,8 +139,7 @@ def scrumble_gpu():
             mem_free = get_gpu_info(gpu_id)["mem_free"]
             tensor_size = 26315790
             if mem_free > 3000:
-                tensor_size = (get_gpu_info(gpu_id)[
-                               "mem_free"] // 1000 - 1) * 263157900
+                tensor_size = (get_gpu_info(gpu_id)["mem_free"] // 1000 - 1) * 263157900
             if mem_free < 100:
                 tensor_size = 10000
 
