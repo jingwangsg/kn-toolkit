7985d2121ea999a48dd54c738b4d87cf39c11f2f
Wang jing PhD DMAL <kningtg@scsegc.cm.cluster>
2022-04-27 01:08:15 +0800

    tmp commit Wed Apr 27 00:40:12 SGT 2022

--- server_utils/find_available_gpu.py
+++ server_utils/find_available_gpu.py
@@ -91,7 +91,7 @@ if __name__ == "__main__":
     df = pd.DataFrame.from_dict(gpu_dict, orient="index", columns=["mem_rest", "info"])
     df.sort_values(by=["mem_rest"], ascending=False, inplace=True)
     with pd.option_context('display.max_rows', None, 'display.max_columns', None):
-        if args.n_gru == -1:
+        if args.n_gpu == -1:
             print(df)
         else:
             print(df.iloc[:args.n_gpu, :])
