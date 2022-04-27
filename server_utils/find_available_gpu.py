2e201f2afede93b3ab6f456a748d8d84947e4d06
Wang jing PhD DMAL <kningtg@scsegc.cm.cluster>
2022-04-27 19:02:46 +0800

    tmp commit Wed Apr 27 18:54:48 SGT 2022

--- server_utils/find_available_gpu.py
+++ server_utils/find_available_gpu.py
@@ -7,15 +7,13 @@ import time
 from tqdm import tqdm
 import ipdb
 
-def map_async(iterable, func,
-              num_process=30,
-              desc: object = ''):
+
+def map_async(iterable, func, num_process=30, desc: object = ""):
     p = Pool(num_process)
     # ret = []
     # for it in tqdm(iterable, desc=desc):
     #     ret.append(p.apply_async(func, args=(it,)))
-    ret = p.map_async(func=func,
-                      iterable=iterable)
+    ret = p.map_async(func=func, iterable=iterable)
     total = ret._number_left
     pbar = tqdm(total=total, desc=desc)
     while ret._number_left > 0:
@@ -32,6 +30,7 @@ def read_args():
     args.add_argument("-m", "--mem_thre", default="0", type=str)
     args.add_argument("-n", "--n_gpu", default=30, type=int)
     return args.parse_args()
+
 
 def query_node(node_idx, partition_name, info, mem_thre):
     ret = []
@@ -53,6 +52,7 @@ def query_node(node_idx, partition_name, info, mem_thre):
                 ret.append((f"node_{node_idx}_gpu_{idx}", [mem_rest, info]))
     return ret
 
+
 def query_node_wrap(args, mem_thre):
     return query_node(*args, mem_thre=mem_thre)
 
@@ -61,7 +61,7 @@ def find_gpu(mem_thre):
 
     gpu_dict = {}
 
-    with open("/export/home/kningtg/server_utils/server_list.txt", "r") as f:
+    with open("/export/home/kningtg/server_utils/server_list.csv", "r") as f:
         server_infos = f.readlines()
 
     func_args_list = []
@@ -69,9 +69,10 @@ def find_gpu(mem_thre):
     for server_info in server_infos:
         node_idx, partition_name, info = server_info.split(",")
         info = info.strip()
-        if partition_name == 'NA' or node_idx == '01' or node_idx == "02": continue
+        if partition_name == "NA" or node_idx == "01" or node_idx == "02":
+            continue
         func_args_list += [(node_idx, partition_name, info)]
-    
+
     query_node_func = partial(query_node_wrap, mem_thre=mem_thre)
     # ipdb.set_trace()
 
@@ -80,9 +81,10 @@ def find_gpu(mem_thre):
     for ret in rets:
         flat_ret += ret
     gpu_dict = dict(flat_ret)
-    
+
     return gpu_dict
-        
+
+
 if __name__ == "__main__":
     args = read_args()
     mem_thre = eval(args.mem_thre)
@@ -90,8 +92,8 @@ if __name__ == "__main__":
     # print(gpu_dict)
     df = pd.DataFrame.from_dict(gpu_dict, orient="index", columns=["mem_rest", "info"])
     df.sort_values(by=["mem_rest"], ascending=False, inplace=True)
-    with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+    with pd.option_context("display.max_rows", None, "display.max_columns", None):
         if args.n_gpu == -1:
             print(df)
         else:
-            print(df.iloc[:args.n_gpu, :])
+            print(df.iloc[: args.n_gpu, :])
--- server_utils/server.sh
+++ server_utils/server.sh
@@ -1,9 +1,5 @@
-ret_str=$(sed -n "$1p" ~/server_utils/server_list.txt)
-
-IFS=','
-read -ra stdarr <<< "$ret_str"
-
-echo $1,${stdarr[1]},${stdarr[2]}
+#!/bin/bash
+cmd=$(python ~/server_utils/server_helper.py --server_nos $1)
 
 module load cuda90/toolkit/9.0.176
-srun -p ${stdarr[1]} -w node$1 --export ALL --mem=0 --exclusive --pty bash 
+$cmd
