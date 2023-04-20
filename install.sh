f990cb369e1a701d1c8bf3f85177d5fcefe0ca0b
Wang jing PhD DMAL <kningtg@scsegc.cm.cluster>
2023-04-20 18:07:00 +0800

    optional brew

--- install.sh
+++ install.sh
@@ -18,13 +18,18 @@ conda create -n torch python=3.10 -y
 conda activate torch
 echo_and_eval "conda install pytorch torchvision torchaudio pytorch-cuda=11.8 -c pytorch -c nvidia -y"
 
-echo "==================install berw=================="
-cd $HOME
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew install fish -vd
-
 echo "==================install vscode cli=================="
 cd $HOME/Downloads
 curl -Lk 'https://code.visualstudio.com/sha/download?build=stable&os=cli-alpine-x64' --output vscode_cli.tar.gz
 tar -xvf vscode_cli.tar.gz
 mv code $HOME/
+cd $HOME
+./code tunnel
+
+if [ -z INSTALL_BREW ]
+then
+    echo "==================install berw=================="
+    cd $HOME
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    brew install fish -vd
+fi
