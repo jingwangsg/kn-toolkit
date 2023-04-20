3273c9c50ce357ed28af0c9993c040dc0faf3c94
Wang jing PhD DMAL <kningtg@scsegc.cm.cluster>
2023-04-20 18:17:05 +0800

    adjust install.sh

--- install.sh
+++ install.sh
@@ -5,11 +5,25 @@ echo_and_eval() {
     eval "$@"
 }
 
-echo "==================install miniconda=================="
-mkdir -p $HOME/Downloads
-cd $HOME/Downloads
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-bash Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/miniconda3
+if [ -e $HOME/code ]
+then
+    echo "==================install vscode cli=================="
+    cd $HOME/Downloads
+    curl -Lk 'https://code.visualstudio.com/sha/download?build=stable&os=cli-alpine-x64' --output vscode_cli.tar.gz
+    tar -xvf vscode_cli.tar.gz
+    mv code $HOME/
+    cd $HOME
+    ./code tunnel
+fi
+
+if [ ! -d $HOME/miniconda3 ]
+then
+    echo "==================install miniconda=================="
+    mkdir -p $HOME/Downloads
+    cd $HOME/Downloads
+    wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+    bash Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/miniconda3
+fi
 
 echo "==================setup env=================="
 conda init bash
@@ -17,14 +31,6 @@ source $HOME/.bashrc
 conda create -n torch python=3.10 -y
 conda activate torch
 echo_and_eval "conda install pytorch torchvision torchaudio pytorch-cuda=11.8 -c pytorch -c nvidia -y"
-
-echo "==================install vscode cli=================="
-cd $HOME/Downloads
-curl -Lk 'https://code.visualstudio.com/sha/download?build=stable&os=cli-alpine-x64' --output vscode_cli.tar.gz
-tar -xvf vscode_cli.tar.gz
-mv code $HOME/
-cd $HOME
-./code tunnel
 
 if [ -z INSTALL_BREW ]
 then
