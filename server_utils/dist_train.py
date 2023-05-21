c5c4c82eccbf54aab4af22fbc9662a0f0ec6a954
Wang jing PhD DMAL <kningtg@scsegc.cm.cluster>
2023-05-21 00:33:32 +0000

    modify server utils

--- server_utils/dist_train.py
+++ server_utils/dist_train.py
@@ -46,7 +46,8 @@ def main():
             rand_left = 1.0
         dataset = np.random.choice(fake_datasets, size=1)
         model = np.random.choice(fake_models, size=1)
-        CUDA_VERSION = subprocess.check_output("cat $HOME/WORKSPACE/kn-toolkit/dotfiles/variable.sh | grep CUDA_VERSION", shell=True).decode("utf-8").strip().split("=")[1]
+        # CUDA_VERSION = subprocess.check_output("cat $HOME/WORKSPACE/kn-toolkit/dotfiles/variable.sh | grep CUDA_VERSION", shell=True).decode("utf-8").strip().split("=")[1]
+        CUDA_VERSION = "10"
         cmd = f"$HOME/miniconda3/envs/cuda{CUDA_VERSION}/bin/python main.py --model {model[0]} --dataset {dataset[0]} --use_bbox_refine --num_epoch 100 --host 127.0.0.1 --ddp --local-rank {id}"
         cmd = cmd + f" {mode_dict[args.mode]} {args.time} {args.mem} {rand_left}"
         subprocess.Popen(cmd, shell=True)
