1f3e448731926f804d8d18929abdbd11b5eeb517
k.nick <kningtg@gmail.com>
2023-09-18 13:43:19 +0800

    add peace mode

--- server_utils/dist_train.py
+++ server_utils/dist_train.py
@@ -1,7 +1,6 @@
 import argparse
 import os.path as osp
 import subprocess
-import torch
 import numpy as np
 import time
 import random
@@ -23,13 +22,25 @@ def parse_args():
     return parser.parse_args()
 
 
+def get_vailable_memory():
+    cmd = "nvidia-smi --query-gpu=gpu_name,memory.total,memory.free --format=csv,noheader"
+    outs = subprocess.run(cmd, shell=True, text=True, capture_output=True).stdout.strip().split("\n")
+    memories = [outs[i].split(",")[-2:] for i in range(len(outs))]
+
+    def _parse_value(mem):
+        return int(mem.strip().split(" ")[0])
+
+    memory_values = [(_parse_value(_[0]), _parse_value(_[1])) for _ in memories]
+    return memory_values
+
+
 def main():
     args = parse_args()
     print(f"Running in mode[{args.mode}] | delay = {args.delay:.1f} (h)")
     time.sleep((args.delay * 3600))
     gpus = []
     if args.gpus == "-1":
-        num_devices = torch.cuda.device_count()
+        num_devices = len(get_vailable_memory())
         gpus = [_ for _ in range(num_devices)]
     else:
         gpus = eval(f"[{args.gpus}]")
@@ -39,7 +50,7 @@ def main():
 
     mode_dict = dict(occupy=0, attack=1, peace=2)
 
-    for id in gpus:
+    def launch(id):
         if args.mode != "attack":
             rand_left = random.uniform(2.5, 3.5)
         else:
@@ -51,6 +62,20 @@ def main():
         cmd = f"$HOME/miniconda3/envs/cuda{CUDA_VERSION}/bin/python main.py --model {model[0]} --dataset {dataset[0]} --use_bbox_refine --num_epoch 100 --host 127.0.0.1 --ddp --local-rank {id}"
         cmd = cmd + f" {mode_dict[args.mode]} {args.time} {args.mem} {rand_left}"
         subprocess.Popen(cmd, shell=True)
+
+    launched = [False for _ in range(len(gpus))]
+    if args.mode == "peace":
+        memories = get_vailable_memory()
+        while (not all(launched)):
+            for id in gpus:
+                if not launched[id] and memories[id][1] / memories[id][0] > 0.5:
+                    launch(id)
+                    launched[id] = True
+            from time import sleep
+            sleep(1)
+    else:
+        for id in gpus:
+            launch(id)
 
     if args.sleep:
         while True:
