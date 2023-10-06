8d1cda4b778d868a3e9c7e79a1e2f404038ffa0f
k.nick <kningtg@gmail.com>
2023-10-06 21:48:57 +0800

    add async download

--- kn_util/tools/lfs.py
+++ kn_util/tools/lfs.py
@@ -4,17 +4,15 @@ import os
 sys.path.insert(0, os.getcwd())
 import subprocess
 import argparse
+import os.path as osp
+from ..utils.download import Downloader
+
+HF_DOWNLOAD_TEMPLATE = "https://huggingface.co/{org}/{repo}/resolve/main/{path}"
 
 
 def parse_args():
     parser = argparse.ArgumentParser(description='tools for lfs')
     parser.add_argument("command", type=str, help="The command to run")
-    return parser
-
-
-def add_pull_args(parser):
-    parser.add_argument("--chunk", type=int, help="The chunk number to fetch", default=100)
-    parser.add_argument("--include", type=str, help="The partial path to fetch, split by ,", default=None)
     return parser
 
 
@@ -26,26 +24,29 @@ def run_cmd(cmd, return_output=False):
         subprocess.run(cmd, shell=True, check=True)
 
 
-def pull(parser):
-    parser = add_pull_args(parser)
-    args = parser.parse_args()
+def lfs_list_files(include=None):
     cmd = "git lfs ls-files"
-    if args.include:
+    if include:
         cmd += " --include=\"{}\"".format(args.include)
     paths = run_cmd(cmd, return_output=True).splitlines()
     paths = [_.split(" - ")[-1] for _ in paths]
+    return paths
 
-    print(f"Found {len(paths)} files to fetch")
+
+def pull(args):
+    paths = lfs_list_files(include=args.include)
+
+    print(f"=> Found {len(paths)} files to fetch")
 
     for idx in range(0, len(paths), args.chunk):
-        print(f"Fetching chunk {idx//args.chunk} of {len(paths)//args.chunk}")
+        print(f"=> Fetching chunk {idx//args.chunk} of {len(paths)//args.chunk}")
         cmd = "git lfs fetch --include=\"{}\"".format(",".join(paths[idx:idx + 100]))
         run_cmd(cmd)
 
     run_cmd("git lfs checkout")
 
 
-def track(parser):
+def track(args):
     # args = parse_args()
     cmd = "find ./ -name '*' -type f -not -path './.git*'"
 
@@ -61,12 +62,51 @@ def track(parser):
         cmd = "git lfs track \"{}\"".format(path)
         run_cmd(cmd)
 
+def _parse_repo_url(url):
+    """parse org, repo from url
+    url like https://huggingface.co/TheBloke/stable-vicuna-13B-GGUF
+    """
+    org, repo = url.split("/")[-2:]
+
+    return org, repo
+
+def download(args):
+
+    # clone the repo
+    run_cmd(f"GIT_LFS_SKIP_SMUDGE=1 git clone {args.url}")
+    folder_name = osp.basename(args.url)
+    if folder_name.endswith(".git"):
+        folder_name = folder_name[:-4]
+    os.chdir(folder_name)
+
+    paths = lfs_list_files(include=args.include)
+    print(f"=> Found {len(paths)} files to download")
+
+    org, repo = _parse_repo_url(args.url)
+
+    for path in paths:
+        if osp.exists(path):
+            os.remove(path)
+
+        url = args.template.format(org=org, repo=repo, path=path)
+        Downloader.async_sharded_download(url, verbose=True)
+
 
 if __name__ == "__main__":
     parser = parse_args()
     command = parser.parse_known_args()[0].command
 
     if command == "pull":
-        pull(parser)
+        parser.add_argument("--chunk", type=int, help="The chunk number to fetch", default=100)
+        parser.add_argument("--include", type=str, help="The partial path to fetch, split by ,", default=None)
+        args = parser.parse_args()
+        pull(args)
     elif command == "track":
-        track(parser)
+        args = parser.parse_args()
+        track(args)
+    elif command == "download":
+        parser.add_argument("url", type=str, help="The url to download")
+        parser.add_argument("--include", type=str, help="The partial path to fetch, split by ,", default=None)
+        parser.add_argument("--template", type=str, help="The chunk number to fetch", default=HF_DOWNLOAD_TEMPLATE)
+        args = parser.parse_args()
+        download(args)
---
+++ kn_util/utils/download.py
@@ -0,0 +1,149 @@
+import asyncio
+import httpx
+import requests
+from tqdm.asyncio import tqdm_asyncio
+from tqdm import tqdm
+from huggingface_hub.utils._headers import build_hf_headers
+from transformers.utils.hub import http_user_agent
+from contextlib import nullcontext
+
+import nest_asyncio
+
+nest_asyncio.apply()
+
+
+def get_head_with_redirects(url, verbose=False, headers=None):
+    with httpx.Client(follow_redirects=False) as client:
+        response = client.head(url, headers=headers)
+        while response.status_code in (301, 302):
+            if verbose:
+                print("Redirected to:", response.headers['Location'])
+            response = client.head(response.headers['Location'])
+        return response
+
+
+# async download
+def get_headers(from_hf=False):
+    # only for huggingface
+    user_agent_header = http_user_agent()
+    if from_hf:
+        headers = build_hf_headers(user_agent=user_agent_header, token="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")
+    else:
+        headers = {"User-Agent": user_agent_header}
+    return headers
+
+
+class Downloader:
+
+    @staticmethod
+    async def _async_range_download(url, save_name, s_pos, e_pos, client, chunk_size, headers=None, pbar=None):
+        range_headers = {"Range": f"bytes={s_pos}-{e_pos}"}
+        if headers:
+            range_headers.update(headers)
+        headers = range_headers
+
+        f = open(save_name, "rb+")
+        async with client.stream('GET', url=url, headers=headers) as r:
+            f.seek(s_pos)
+            async for chunk in r.aiter_bytes():
+                if chunk:  # prevent keep-alive chunks
+                    f.write(chunk)
+                    if pbar:
+                        pbar.update(len(chunk))
+        f.close()
+
+    @staticmethod
+    def _calc_divisional_range(filesize, chuck=10):
+        step = filesize // chuck
+        arr = list(range(0, filesize, step))
+        result = []
+        for i in range(len(arr) - 1):
+            s_pos, e_pos = arr[i], arr[i + 1] - 1
+            result.append([s_pos, e_pos])
+        result[-1][-1] = filesize - 1
+        return result
+
+    @classmethod
+    def async_sharded_download(cls,
+                               url,
+                               save_name=None,
+                               chunk_size=1024 * 100,
+                               num_shards=10,
+                               headers=None,
+                               verbose=True):
+        if save_name is None:
+            save_name = url.split("/")[-1]
+
+        # resolve redirect
+        res = get_head_with_redirects(url, headers=headers)
+
+        if res.headers.get("Accept-Ranges", None) != "bytes":
+            print("File does not support range download, use direct download")
+            cls.download(url, save_name, headers=headers, verbose=verbose)
+            return
+
+        # get filesize
+        url = res.url
+        filesize = int(res.headers["Content-Length"])
+        divisional_ranges = cls._calc_divisional_range(filesize, num_shards)
+
+        with open(save_name, "wb") as f:
+            pass
+
+        transport = httpx.AsyncHTTPTransport(retries=5)
+        client = httpx.AsyncClient(transport=transport, timeout=60)
+        loop = asyncio.get_event_loop()
+
+        pbar = tqdm_asyncio(total=filesize,
+                            dynamic_ncols=True,
+                            desc=f"Downloading {save_name}",
+                            unit="B",
+                            unit_scale=True,
+                            smoothing=0.1,
+                            miniters=1,
+                            ascii=True) if verbose else None
+        context = pbar if verbose else nullcontext()
+
+        with context:
+            tasks = [
+                cls._async_range_download(url,
+                                          save_name,
+                                          s_pos,
+                                          e_pos,
+                                          chunk_size=chunk_size,
+                                          headers=headers,
+                                          client=client,
+                                          pbar=pbar) for s_pos, e_pos in divisional_ranges
+            ]
+            loop.run_until_complete(asyncio.wait(tasks))
+
+    @classmethod
+    def download(cls, url, chunk_size=1024 * 100, save_name=None, headers=None, verbose=True):
+        if save_name is None:
+            save_name = url.split("/")[-1]
+
+        # resolve redirect
+        res = get_head_with_redirects(url, headers=headers)
+        url = res.url
+        filesize = int(res.headers["Content-Length"])
+
+        pbar = tqdm(total=filesize,
+                    dynamic_ncols=True,
+                    desc=f"Downloading {save_name}",
+                    unit="B",
+                    unit_scale=True,
+                    smoothing=0.1,
+                    miniters=1,
+                    ascii=True) if verbose else None
+
+        context = pbar if verbose else nullcontext()
+
+        with context:
+            client = httpx.Client()
+            with client.stream('GET', url=url, headers=headers) as r:
+                with open(save_name, "wb") as f:
+                    for chunk in r.iter_bytes(chunk_size=chunk_size):
+                        if chunk:
+                            f.write(chunk)
+                            if pbar:
+                                pbar.update(len(chunk))
