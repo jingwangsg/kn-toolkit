29ecd7de517bebdbfa3798cd9d4d4df8e12f65c6
k.nick <kningtg@gmail.com>
2024-02-19 06:42:13 +0800

    isolate utils

--- kn_util/utils/__init__.py
+++ kn_util/utils/__init__.py
@@ -1,12 +1,1 @@
 # from .checkpoint import CheckPointer
-from .ops import clones, detach_collections
-from .git_utils import commit, get_origin_url
-from .output import explore_content, dict2str, max_memory_allocated, lazyconf2str
-from .mail import send_email
-from .download import *
-from .system import run_cmd
-from .cache import cached_func
-from .misc import seed_everything
-from .io import (load_json, save_json, load_pickle, save_pickle, load_csv, load_hdf5, save_hdf5, load_jsonl,
-                 save_jsonl)
-from .logger import setup_logger_loguru, SmoothedValue, MetricLogger
