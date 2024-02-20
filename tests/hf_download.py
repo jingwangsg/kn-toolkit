82ac1e3758b1c3bf5ffcf41ce7ddbe617c35e4c2
nickymouse <kningtg@gmail.com>
2024-02-20 15:54:44 +0800

    fix hf auth and filesize xlink

--- tests/hf_download.py
+++
@@ -1,4 +0,0 @@
-from transformers import AutoModelForCausalLM
-
-model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-70b-hf", token="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")
-import ipdb; ipdb.set_trace()
