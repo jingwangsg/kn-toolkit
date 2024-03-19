2d9b63bf365058e5fedba6fef4941d74282459f4
kningtg <kningtg@gmail.com>
2024-03-19 21:03:18 +0800

    add wids

--- kn_util/data/collate.py
+++ kn_util/data/collate.py
@@ -1,13 +1,13 @@
 from torch.utils.data import default_collate
 
 
-def default_collate_builder(default_keys, list_keys=None):
+def default_collate_builder(default_keys=[], list_keys=[]):
     def default_collate_wrapped(batch):
         ret = {}
-        batch = [{k: v for k, v in item.items() if k in default_keys} for item in batch]
+        batch = [{k: v for k, v in item.items() if k in default_keys or k in list_keys} for item in batch]
         for key in default_keys:
             ret[key] = default_collate([item[key] for item in batch])
-        for key in list_keys or []:
+        for key in list_keys:
             ret[key] = [item[key] for item in batch]
         return ret
 
