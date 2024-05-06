ce8160337083763f08bd19bb67e1091f5cda0cbe
wangjing <wangjing@sea.com>
2024-05-06 11:17:37 +0800

    support command line for email

--- kn_util/tools/email.py
+++ kn_util/tools/email.py
@@ -7,21 +7,32 @@ def main():
     hostname = run_cmd("hostname").stdout.strip()
     DEFAULT_BODY = [
         "Hostname: " + hostname,
-        "GPU Info: ",
-        run_cmd("nvidia-smi").stdout,
     ]
+
+    nvidia_output = run_cmd("nvidia-smi")
+    if nvidia_output.returncode != 0:
+        DEFAULT_BODY += [
+            "GPU not found",
+        ]
+    else:
+        DEFAULT_BODY += [
+            "GPU Information:",
+            nvidia_output.stdout,
+        ]
+
+    DEFAULT_BODY =  ["---------------Meta Information----------------"] + DEFAULT_BODY
+    DEFAULT_BODY += ["----------------------------------------------------"]
 
     parser = argparse.ArgumentParser(description="Send email")
     parser.add_argument("--to", type=str, help="To email address")
     parser.add_argument("--subject", type=str, help="Subject of the email", default=f"Message from GPU Cluster ({hostname})")
-    parser.add_argument("-m", "--message", type=str, help="Body of the email", default=None)
+    parser.add_argument("-m", "--message", help="Body of the email", nargs="+", default=[], type=str)
     args = parser.parse_args()
 
-    if args.message is not None:
-        DEFAULT_BODY.append(args.message)
+    message = DEFAULT_BODY + args.message
 
     send_email(
         to_addr="kningtg@gmail.com",
         subject=args.subject,
-        text="\n".join(DEFAULT_BODY),
+        text="\n".join(message),
     )
