81f065a60eada949544bbd10b5db0168518ac3f7
wangjing <wangjing@sea.com>
2024-05-07 13:58:50 +0800

    compare modified time before rsync

--- kn_util/tools/rsync.py
+++ kn_util/tools/rsync.py
@@ -1,9 +1,20 @@
 import argparse
 import subprocess
-import os
-from ..utils.multiproc import map_async, map_async_with_thread
-import os.path as osp
+import os, os.path as osp
+from datetime import datetime
+
 from ..utils.system import run_cmd
+from ..utils.multiproc import map_async, map_async_with_thread
+
+
+def run_cmd_remote_maybe(cmd, hostname=None):
+    def cmd_on_ssh(hostname, cmd):
+        return f"ssh {hostname} '{cmd}'"
+
+    cur_hostname = run_cmd("hostname").stdout.strip()
+    if hostname is not None and hostname != cur_hostname:
+        cmd = cmd_on_ssh(hostname, cmd)
+    return run_cmd(cmd)
 
 
 def split_path(path):
@@ -18,12 +29,66 @@ def cmd_get_path(path):
     return f"readlink -f {path}"
 
 
-def cmd_on_ssh(hostname, cmd):
-    return f"ssh {hostname} '{cmd}'"
-
-
 def cmd_ssh_relay(from_user, from_ip, to_ip, cmd, port=9999, to_port=22):
     return f"ssh -R 127.0.0.1:{port}:{to_ip}:{to_port} {from_user}@{from_ip} '{cmd}'"
+
+
+def check_hostname_available(hostname):
+    returncode = run_cmd_remote_maybe("echo 1", hostname).returncode
+    return returncode == 0
+
+
+def prepare_path_for_rsync(hostname=None, path=None):
+    """path can be a list of paths or a single path"""
+
+    def _combine(hostname, path):
+        return f"{hostname}:{path}" if hostname is not None else path
+
+    is_remote = hostname is not None
+
+    path_delimiter = " :" if is_remote else " "
+
+    if isinstance(path, list):
+        return _combine(hostname, path_delimiter.join([f"'{it_path}'" for it_path in path]))
+    else:
+        return _combine(hostname, path)
+
+
+def get_last_modified(path, hostname):
+    # only support linux so far
+    last_modified = run_cmd_remote_maybe(
+        f'find {path} -type f -printf "%-.22T+ %M %n %-8u %-8g %8s %Tx %.8TX %p\n" | sort | tail -1' , hostname
+    ).stdout.strip().split(" ")[0]
+    # example format 2024-05-06+18:42:28.58
+    last_modified = datetime.strptime(last_modified, "%Y-%m-%d+%H:%M:%S.%f")
+
+    return last_modified
+
+
+def cmd_rsync(
+    to_host,
+    to_path,
+    from_host,
+    from_path,
+    relative=False,
+    remove_source_files=False,
+    exclude=None,
+):
+    rsync_args = "-auvP"
+
+    if relative:
+        rsync_args += " --relative "
+
+    if remove_source_files:
+        rsync_args += " --remove-source-files "
+
+    if exclude is not None:
+        rsync_args += f" --exclude={exclude} "
+
+    from_path_for_rsync = prepare_path_for_rsync(hostname=from_host, path=from_path)
+    to_path_for_rsync = prepare_path_for_rsync(hostname=to_host, path=to_path)
+
+    return f"rsync {rsync_args} {from_path_for_rsync} {to_path_for_rsync}"
 
 
 def parse(s):
@@ -36,7 +101,7 @@ def parse(s):
         dir_path = s
 
     if hostname is not None:
-        dir_path = run_cmd(cmd_on_ssh(hostname, cmd_get_path(dir_path))).stdout.strip()
+        dir_path = run_cmd_remote_maybe(cmd_get_path(dir_path), hostname).stdout.strip()
     else:
         dir_path = osp.realpath(dir_path)
 
@@ -47,54 +112,6 @@ def parse(s):
 
 
 class RsyncTool:
-
-    @staticmethod
-    def prepare_path_for_rsync(hostname=None, path=None):
-        """path can be a list of paths or a single path"""
-
-        def _combine(hostname, path):
-            return f"{hostname}:{path}" if hostname is not None else path
-
-        is_remote = hostname is not None
-
-        path_delimiter = " :" if is_remote else " "
-
-        if isinstance(path, list):
-            return _combine(hostname, path_delimiter.join([f"'{it_path}'" for it_path in path]))
-        else:
-            return _combine(hostname, path)
-
-    @classmethod
-    def cmd_rsync(
-        cls,
-        to_host,
-        to_path,
-        from_host,
-        from_path,
-        relative=False,
-        remove_source_files=False,
-        exclude=None,
-    ):
-        rsync_args = "-auvP"
-
-        if relative:
-            rsync_args += " --relative "
-
-        if remove_source_files:
-            rsync_args += " --remove-source-files "
-
-        if exclude is not None:
-            rsync_args += f" --exclude={exclude} "
-
-        from_path_for_rsync = cls.prepare_path_for_rsync(hostname=from_host, path=from_path)
-        to_path_for_rsync = cls.prepare_path_for_rsync(hostname=to_host, path=to_path)
-
-        return f"rsync {rsync_args} {from_path_for_rsync} {to_path_for_rsync}"
-
-    @staticmethod
-    def check_hostname_available(hostname):
-        returncode = run_cmd(cmd_on_ssh(hostname, "echo 1")).returncode
-        return returncode == 0
 
     @classmethod
     def delete(cls, dir_path):
@@ -122,9 +139,16 @@ class RsyncTool:
         to_host, to_path = parse(to_addr)
 
         if from_host is not None:
-            assert cls.check_hostname_available(from_host), f"hostname {from_host} not available"
+            assert check_hostname_available(from_host), f"hostname {from_host} not available"
         if to_host is not None:
-            assert cls.check_hostname_available(to_host), f"hostname {to_host} not available"
+            assert check_hostname_available(to_host), f"hostname {to_host} not available"
+        
+        import ipdb; ipdb.set_trace()
+
+        if get_last_modified(from_path, from_host) >= get_last_modified(to_path, to_host):
+            continue_flag = input("from_path is newer than to_path, continue? (y/n)")
+            if continue_flag != "y":
+                return
 
         num_remotes = (from_host is not None) + (to_host is not None)
         if num_remotes == 2:
@@ -135,28 +159,24 @@ class RsyncTool:
 
         print("=> Using {} - {}".format(from_mode, to_mode))
 
-        def construct_cmd(from_path_):
-            return cls.cmd_rsync(
+        if not async_dir:
+            cmd = cmd_rsync(
                 to_host=to_host,
                 to_path=to_path,
                 from_host=from_host,
-                from_path=from_path_,
-                relative=async_dir,
+                from_path=from_path,
                 **rsync_kwargs,
             )
-
-        if not async_dir:
-            cmd = construct_cmd(from_path)
             print(cmd)
             subprocess.run(cmd, shell=True)
         else:
             # list all files recursively in from_path
             def list_files(from_path, size="+1g"):
                 cmd = cmd_list_files(from_path, size=size)
-                if from_mode == "remote":
-                    cmd = cmd_on_ssh(from_host, cmd)
 
-                out = run_cmd(cmd).stdout.strip()
+                # out = run_cmd(cmd).stdout.strip()
+                out = run_cmd_remote_maybe(cmd, from_host).stdout.strip()
+
                 from_files = out.split("\n")
                 from_files = [x for x in from_files if len(x.strip()) > 0 and path_filter(x)]
                 from_paths = [osp.join(from_path, ".", fn) for fn in from_files]
@@ -166,26 +186,31 @@ class RsyncTool:
 
             print("=> using async dir")
 
-            def rsync_in_chunk(paths, chunk_size, desc="Rsync"):
-                # construct chunks
-                path_chunks = [paths[i : i + chunk_size] for i in range(0, len(paths), chunk_size)]
-
-                def _apply(path_chunk):
-                    cmd = construct_cmd(path_chunk)
-                    run_cmd(cmd, verbose=False, async_cmd=False)
-
-                map_async_with_thread(
-                    func=_apply,
-                    iterable=path_chunks,
-                    num_thread=num_thread,
-                    desc=desc,
-                )
+            rsync_kwargs["to_host"] = to_host
+            rsync_kwargs["to_path"] = to_path
+            rsync_kwargs["from_host"] = from_host
 
             file_small = list_files(from_path, size="-512m")
-            rsync_in_chunk(file_small, chunk_size=chunk_size, desc="Rsync Small")
+            cls.rsync_in_chunk(file_small, chunk_size=chunk_size, num_thread=num_thread, desc="Rsync Small", **rsync_kwargs)
 
             files_large = list_files(from_path, size="+512m")
-            rsync_in_chunk(files_large, chunk_size=1, desc="Rsync Large")
+            cls.rsync_in_chunk(files_large, chunk_size=1, num_thread=num_thread, desc="Rsync Large", **rsync_kwargs)
+
+    @classmethod
+    def rsync_in_chunk(cls, paths, chunk_size, num_thread=32, desc="Rsync", **rsync_kwargs):
+        # construct chunks
+        path_chunks = [paths[i : i + chunk_size] for i in range(0, len(paths), chunk_size)]
+
+        def _apply(path_chunk):
+            cmd = cmd_rsync(from_path=path_chunk, **rsync_kwargs)
+            run_cmd(cmd, verbose=False, async_cmd=False)
+
+        map_async_with_thread(
+            func=_apply,
+            iterable=path_chunks,
+            num_thread=num_thread,
+            desc=desc,
+        )
 
 
 def main():
@@ -197,18 +222,8 @@ def main():
     if command == "launch":
         parser.add_argument("from_dir", type=str, help=format_help)
         parser.add_argument("to_dir", type=str, help=format_help)
-        parser.add_argument(
-            "--async-dir",
-            default=False,
-            action="store_true",
-            help="async download/upload dir",
-        )
-        parser.add_argument(
-            "--chunk-size",
-            default=30,
-            type=int,
-            help="chunk size for async dir",
-        )
+        parser.add_argument("--async-dir", default=False, action="store_true", help="async download/upload dir")
+        parser.add_argument("--chunk-size", default=30, type=int, help="chunk size for async dir")
         parser.add_argument("-n", "--num-thread", type=int, default=16)
         parser.add_argument("--paths", type=str, help="python expression to generate paths", default=None)
 
@@ -225,6 +240,7 @@ def main():
             chunk_size=args.chunk_size,
             num_thread=args.num_thread,
         )
+
     elif command == "delete":
         parser.add_argument("dir_path", type=str, help="dir to delete")
         args = parser.parse_args()
